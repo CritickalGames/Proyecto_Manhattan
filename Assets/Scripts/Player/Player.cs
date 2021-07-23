a8f0e497b985080f6dc63ebfa75c5ff97d544231
xnusef <intifernandez2004@gmail.com>
2021-07-22 23:06:07 -0300

    AtackNISDone

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,11 +4,13 @@ public class Player : MonoBehaviour
 {
     [System.NonSerialized]public PlayerInput inputScript;
     [System.NonSerialized]public PlayerMovement movementScript;
+    [System.NonSerialized]public PlayerAtack atackScript;
 
     void Start()
     {
         inputScript = GetComponent<PlayerInput>();
         movementScript = GetComponent<PlayerMovement>();
+        atackScript = GetComponent<PlayerAtack>();
     }
 
     
