57b9bd44b0e09a88d21d2352bdbf54412a1198a8
TEX APlayer <intifernandez2004@gmail.com>
2021-08-29 21:36:25 -0300

    TMP imported and not much more

---
+++ Assets/Scripts/Management/Data/ConfigData.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[System.Serializable] public class ConfigData 
+{
+    public int resolutionIndex;
+    public int qualityIndex;
+
+    public ConfigData()
+    {
+        
+    }
+}
