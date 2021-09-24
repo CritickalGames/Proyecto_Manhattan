f6e126ddceb91d7df7ffce92bf734fd7a9a51500
TEX APlayer <intifernandez2004@gmail.com>
2021-09-23 23:28:01 -0300

    Deleted folders

--- Assets/Scripts/Game/General/AutoDestroy.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
- using System.Collections;
- 
- public class AutoDestroy : MonoBehaviour {
-     void Start () 
-     {
-         Destroy (gameObject, this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length); 
-     }
- }
