8f1ef342c772123041b5dac008e83443296c1d5e
TEX APlayer <intifernandez2004@gmail.com>
2021-09-25 00:00:37 -0300

    rezo por que funcione

--- Assets/Editor/BuildProject.cs
+++ Assets/Editor/BuildProject.cs
@@ -5,10 +5,10 @@ using UnityEditor;
 
 public class BuildProject : MonoBehaviour
 {
-    //public static string version = System.Environment.GetEnvironmentVariable("projectVersion");
-    public static string gameName = System.Environment.GetEnvironmentVariable("gameName"); 
+    public static string version = System.Environment.GetEnvironmentVariable("projectVersion");
+    public static string gameName = System.Environment.GetEnvironmentVariable("gameName");
 
-    public static void BuildWindows64(string version)
+    public static void BuildWindows64()
     {
         string buildPath = ".\\Builds\\" + version + "\\Win64\\" + gameName + ".exe";
         BuildPipeline.BuildPlayer(
@@ -27,7 +27,7 @@ public class BuildProject : MonoBehaviour
             BuildOptions.None
         );
     }
-    public static void BuildWindows32(string version)
+    public static void BuildWindows32()
     {
         string buildPath = ".\\Builds\\" + version + "\\Win32\\" + gameName + ".exe";
         BuildPipeline.BuildPlayer(
