260c488ac3533e4702b6438f5ab7f1e3eb6442f6
TEX APlayer <intifernandez2004@gmail.com>
2021-09-29 21:53:32 -0300

    Finished levelpassing, healing and messages

--- Assets/Scripts/Management/LevelPassed.cs
+++ Assets/Scripts/Management/LevelPassed.cs
@@ -6,18 +6,21 @@ public class LevelPassed : MonoBehaviour
 {
     [SerializeField]private int nextLevel = 0;
     [SerializeField]private string[] UnlockCountry;
+    [SerializeField]private int spawnScene;
+    [SerializeField]private bool healOnPass;
 
-    public void NextLevel()
+    private void Start()
     {
-        LevelManager.lM.NextLevel(nextLevel);
+        LevelManager.lM.spawnScene = this.spawnScene;
     }
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (UnlockCountry != null)
-            for (int i = 0 ; i < UnlockCountry.Length ; i++)
-                LevelManager.lM.UnlockCountry(UnlockCountry[i]);
-
         if (col.gameObject.CompareTag("Player"))
-            LevelManager.lM.NextLevel(nextLevel);
+        {
+            if (this.UnlockCountry != null)
+                for (int i = 0 ; i < this.UnlockCountry.Length ; i++)
+                    LevelManager.lM.UnlockCountry(UnlockCountry[i]);
+            LevelManager.lM.NextLevel(this.nextLevel, this.healOnPass);
+        }
     }
 }
