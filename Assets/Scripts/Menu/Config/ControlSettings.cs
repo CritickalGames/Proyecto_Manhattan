7586fc16f927cf8ddc247f9c9d3d64a24e10668c
TEX APlayer <intifernandez2004@gmail.com>
2021-10-14 09:33:41 -0300

    I thought I had commited

--- Assets/Scripts/Menu/Config/ControlSettings.cs
+++ Assets/Scripts/Menu/Config/ControlSettings.cs
@@ -11,16 +11,8 @@ public class ControlSettings : MonoBehaviour
     private PlayerInput PlayerInput;
     private InputActionRebindingExtensions.RebindingOperation operation;
 
-    void Start()
-    {
-        for (int i = 0; i < displayControl.Length; i++)
-        {
-            int bindingIndex = this.action[i].action.GetBindingIndexForControl(this.action[i].action.controls[0]);
-            this.displayControl[i].text = InputControlPath.ToHumanReadableString(
-                this.action[i].action.bindings[bindingIndex].effectivePath,
-                InputControlPath.HumanReadableStringOptions.OmitDevice);
-        }
-    }
+    void Awake() => this.PlayerInput = InputManager.iM.gameObject.GetComponent<PlayerInput>();
+    void Start() => Load();
     public void StartedRebind(int index)
     {
         this.buttons[index].GetComponent<Button>().interactable = false;
@@ -41,7 +33,40 @@ public class ControlSettings : MonoBehaviour
             this.action[index].action.bindings[bindingIndex].effectivePath,
             InputControlPath.HumanReadableStringOptions.OmitDevice));
         this.operation.Dispose();
-        this.buttons[index].GetComponent<Button>().interactable = true;
+        Button button = this.buttons[index].GetComponent<Button>();
+        button.interactable = true;
+        button.Select();
+        Save();
     }
     private void ChangeText(int index,string text) => this.displayControl[index].text = text;
+    private void Save()
+    {
+        string rebinds = PlayerInput.actions.SaveBindingOverridesAsJson();
+        PlayerPrefs.SetString("rebinds", rebinds);
+    }
+    private void Load()
+    {
+        string rebinds = PlayerPrefs.GetString("rebinds", string.Empty);
+        if (!string.IsNullOrEmpty(rebinds))
+            PlayerInput.actions.LoadBindingOverridesFromJson(rebinds);
+        for (int i = 0; i < displayControl.Length; i++)
+        {
+            int bindingIndex = this.action[i].action.GetBindingIndexForControl(this.action[i].action.controls[0]);
+            this.ChangeText(i, InputControlPath.ToHumanReadableString(
+                this.action[i].action.bindings[bindingIndex].effectivePath,
+                InputControlPath.HumanReadableStringOptions.OmitDevice));
+        }
+    }
+    public void Restore()
+    {
+        PlayerPrefs.DeleteKey("rebinds");
+        //TODO: Actualizar después de resetear
+        for (int i = 0; i < displayControl.Length; i++)
+        {
+            int bindingIndex = this.action[i].action.GetBindingIndexForControl(this.action[i].action.controls[0]);
+            this.ChangeText(i, InputControlPath.ToHumanReadableString(
+                this.action[i].action.bindings[bindingIndex].effectivePath,
+                InputControlPath.HumanReadableStringOptions.OmitDevice));
+        }
+    }
 }
