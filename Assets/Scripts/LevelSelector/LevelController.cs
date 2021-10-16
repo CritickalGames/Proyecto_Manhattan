6f7ed41c26e281907a0897cc21a04b9ab08875a8
TEX APlayer <intifernandez2004@gmail.com>
2021-10-15 22:01:48 -0300

    Started manually mergeing

--- Assets/Scripts/LevelSelector/LevelController.cs
+++ Assets/Scripts/LevelSelector/LevelController.cs
@@ -37,7 +37,7 @@ public class LevelController : MonoBehaviour
     }
     public void LoadScene(int levelScene)
     {
-        SceneManager.LoadScene(levelScene);
+        LevelManager.lM.StartAnim(levelScene);
     }
     public void PlaySound(string sound)
     {
